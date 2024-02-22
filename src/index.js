94ff7b847d262b94081042f59ee96cf7bcf57d04
kishanvish1613 <100762979+kishanvish1613@users.noreply.github.com>
2024-02-22 21:23:13 +0530

    fix env

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import apiRoutes from './routes/index.js'
 
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
